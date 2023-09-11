a79fbc70adbeb2cdd8ac1b665033ea90fe3ddaac
Indrajeet5497 <indrabit4@gmail.com>
2023-09-11 23:01:02 +0530

    card commit

--- script.js
+++ script.js
@@ -1,39 +1,14 @@
-// Here value of a is 10
-var a = 10; 
-// Here value of b is 8
-var b = 8;
-// Here value of c is 6
-var c = 6;
-{
-    let a = 2;
-    // Here value of a is 2 but only within the block
-    let b = 10;
-    // Here value of b is 10 inside and outside the block it will remain 10
-    const c = 8;
-    // Here value of c is 8 but only within the block
-    // const cannot be reassigned
-    // c = 5;
-}
-document.write("The value of a is :" +a);
-// let has block scope
-document.write("<br>");
-document.write("The value of b is :" +b);
-// var does not have block scope
-document.write("<br>");
-document.write("This value of c is :" +c);
-// const has block scope
-document.write("<br>");
+var istatus = document.querySelector("h3")
 
+var addFriend = document.querySelector("#add")
+var removeFriend = document.querySelector("#remove")
 
-class testbook {
-    constructor (g){
-        this.Charactor = g
-    }
-}
-class skill_academy {
-    disp() {
-        console.log("I have a Ford, it is a Mustang :" + this.Charactor);
-    }
-}
-var obj = new skill_academy();
-obj.disp()
+addFriend.addEventListener("click",function(){
+    istatus.innerHTML = "Kirti 18th Sep"
+    istatus.style.color = "green"
+})
+
+removeFriend.addEventListener("click",function(){
+    istatus.innerHTML = "Happy Birthday"
+    istatus.style.color = "red"
+})
